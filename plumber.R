e1729161c501a5cfc219126e0fc338b8e5206d6e
David Mallard <correspondence@davidmallard.id.au>
2018-10-15 09:22:38 +1100

    Initialise as a plumber API project

---
+++ plumber.R
@@ -0,0 +1,22 @@
+#
+# This is a Plumber API. In RStudio 1.2 or newer you can run the API by
+# clicking the 'Run API' button above.
+#
+# In RStudio 1.1 or older, see the Plumber documentation for details
+# on running the API.
+#
+# Find out more about building APIs with Plumber here:
+#
+#    https://www.rplumber.io/
+#
+
+library(plumber)
+
+#* @apiTitle AusVotes API
+
+#* Echo back the input
+#* @param msg The message to echo
+#* @get /echo
+function(msg=""){
+  list(msg = paste0("The message is: '", msg, "'"))
+}
