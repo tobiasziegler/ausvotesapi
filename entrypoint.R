9132a44fb990379af90c730a5f1a163d4b93600f
David Mallard <correspondence@davidmallard.id.au>
2018-10-15 13:58:09 +1100

    Create a connection pool to access a PostgreSQL database

--- entrypoint.R
+++ entrypoint.R
@@ -1,6 +1,17 @@
 library(plumber)
+library(pool)
+library(RPostgres)
 
 config <- config::get()
+
+pool <- pool::dbPool(
+  drv = RPostgres::Postgres(),
+  dbname = config$dbname,
+  host = config$host,
+  port = config$port,
+  user = config$user,
+  password = config$password
+)
 
 router <- plumber::plumb("plumber.R")
 router$run(port = 8000)
